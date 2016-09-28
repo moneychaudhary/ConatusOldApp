d336fe96a9b92b4f1629be619b2f64a7abbe6df7
moneychaudhary <money.chaudhary11@gmail.com>
2016-09-28 10:18:33 +0530

    ButterKnife Integration Done

--- app/src/main/java/com/example/money/conatusapp/MainActivity.java
+++ app/src/main/java/com/example/money/conatusapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.money.conatusapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
